caada6e38ca8f09cba3554596899f963c4ca8a96
artmigalev <art_migalev@yahoo.com>
2026-01-16 18:20:06 +0300

    feat:init login page

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {
+  MatCard,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardContent,
+  MatCardFooter,
+} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardFooter,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export default class Login {}
