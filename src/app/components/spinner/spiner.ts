1a9c3a9281060ab496dbea7f4efbabfdd851ed76
artmigalev <art_migalev@yahoo.com>
2026-01-27 20:32:33 +0300

    feat: spinner

---
+++ src/app/components/spinner/spiner.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+@Component({
+  selector: 'app-spinner',
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './spinner.html',
+  styleUrl: './spinner.scss',
+})
+export class Spinner {
+  isShow = input<boolean>();
+}
