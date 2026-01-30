2e66d1a78701aee29210fa49552fff4e98860d0a
artmigalev <art_migalev@yahoo.com>
2026-01-30 12:32:26 +0300

    fix: with routing

--- src/app/layouts/dashboard/dashboard.ts
+++ src/app/layouts/dashboard/dashboard.ts
@@ -1,33 +1,54 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { AuthService } from '@app/data/services/auth/auth.service';
-import { DashboardService } from '@app/data/services/dashbord.service';
-import { TabsService } from '@app/data/services/tab/tabs.service';
+import { DashboardService } from '@app/data/services/dashboard/dashbord.service';
 import { MessagesForUser } from '@app/shared/messages-for-user.enum';
 import TabSwitcherComponent from '@components/tab-switcher/tab-switcher.';
-import CardListComponent from '@layouts/card-list/card-list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Spinner } from '@app/components/spinner/spiner';
 import { Notify } from '@app/components/notify/notify';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { ApiCallsService } from '@app/data/services/api/api-calls.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { ResponseTabs } from '@app/types/response-tabs.interface';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TabSwitcherComponent, CardListComponent, MatProgressSpinnerModule, Spinner, Notify],
+  imports: [TabSwitcherComponent, MatProgressSpinnerModule, Spinner, Notify, RouterOutlet],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export default class DashboardComponent {
-  tadService = inject(TabsService);
   serviceAuth = inject(AuthService);
   serviceDashboards = inject(DashboardService);
+  serviceApiCalls = inject(ApiCallsService);
+
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+
+  dashboard = toSignal(this.route.data.pipe(map((data) => data['dashboard'] as ResponseTabs)));
+
+  tabs = computed(() => this.dashboard()?.tabs ?? []);
 
   emptyDashboards = computed<boolean>(() => this.serviceDashboards.dashboards().length === 0);
-  dashboard = computed(() => this.serviceDashboards.dashboardResource.value());
 
-  isLoading = computed(() => this.serviceDashboards.dashboardResource.isLoading());
-
-  userMessage = MessagesForUser.EMPTY_STATE_FOR_DASHBOARD;
+  isLoading = computed(() => !!this.router.currentNavigation());
 
   emptyStateMessage = MessagesForUser.EMPTY_STATE_FOR_DASHBOARD;
 
-  tab = computed(() => this.tadService.tab());
+  constructor() {
+    effect(() => {
+      const currentRoute = this.route.firstChild;
+      console.log(currentRoute);
+      if (!currentRoute) {
+        const firstTabId = this.dashboard()?.tabs[0].id;
+        if (firstTabId) {
+          this.router.navigate([firstTabId], {
+            relativeTo: this.route,
+            replaceUrl: true,
+          });
+        }
+      }
+    });
+  }
 }
